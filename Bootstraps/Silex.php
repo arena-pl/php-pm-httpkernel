fc49c1518ce7d50b8e2ebb60fbd11c5189dcbf02
Max Małecki <maxmalecki@gmail.com>
2016-05-16 11:52:45 +0200

    Silex fix

--- Bootstraps/Silex.php
+++ Bootstraps/Silex.php
@@ -47,42 +47,10 @@ class Silex  extends AbstractBootstrap implements HooksInterface
         } elseif (file_exists('./vendor/autoload.php')) {
             require './vendor/autoload.php';
         }
-        if(class_exists('\AppKernel')) {
-            $app = new \AppKernel($this->appenv, $this->debug);
-        } else {
-            if(file_exists('./app/bootstrap.php.cache') && file_exists('./app/AppKernel.php')) {
-                require_once './app/bootstrap.php.cache';
-                require_once './app/AppKernel.php';
-                $app = new \AppKernel($this->appenv, $this->debug);
-            }
 
-
-        }
-        //since we need to change some services, we need to manually change some services
-        //we need to change some services, before the boot, because they would otherwise
-        //be instantiated and passed to other classes which makes it impossible to replace them.
-        Utils::bindAndCall(function () use ($app) {
-            // init bundles
-            $app->initializeBundles();
-
-            // init container
-            $app->initializeContainer();
-        }, $app);
-
-        Utils::bindAndCall(function () use ($app) {
-            foreach ($app->getBundles() as $bundle) {
-                $bundle->setContainer($app->container);
-                $bundle->boot();
-            }
-
-            $app->booted = true;
-        }, $app);
-
-        //warm up
-        $request = new Request();
-        $request->setMethod(Request::METHOD_HEAD);
-        $app->handle($request);
-        $this->postHandle($app);
+        $app = require "./src/app.php";
+        require './config/prod.php';
+        require './src/controllers.php';
 
         return $app;
     }
